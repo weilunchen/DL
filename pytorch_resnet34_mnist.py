9766d9aadb066e680176e18a51708ca98f297436
Marciano Jorden <marci_jorden@msn.com>
2021-03-16 14:22:29 +0100

    Add GPU acceleration

--- pytorch_resnet34_mnist.py
+++ pytorch_resnet34_mnist.py
@@ -35,19 +35,36 @@ preprocess = transforms.Compose([
 	transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+device = torch.device('cpu')
 #Load MNIST data set from torchvision
 #MNIST contains 70.000 images = 60.000 train + 10.000 test
 #Images are all 28 x 28 pixels
 mnist_train = datasets.MNIST(root='./data', train=True, download=True, transform=preprocess)
 mnist_test = datasets.MNIST(root='./data', train=False, download=True, transform=preprocess)
 
+
+# Check whether GPU processing is enabled
+if torch.cuda.is_available():
+    print('CUDA GPU available')
+    device = torch.device('cuda')
+    model.to('cuda')
+else:
+    print('CUDA GPU not available')
+
 #input_batch = mnist_train.unsqueeze(0)
 print("Begin eval")
+
+batch_counter = 0
 for x in enumerate (mnist_train):
+    if batch_counter % 500 == 0:
+        print(f'Started with input batch {batch_counter + 1}')
     input_batch = x[1][0].unsqueeze(0)
+    input_batch = input_batch.to(device=device)
     output = model(input_batch)
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    print(probabilities)
+    #print(probabilities)
+
+    batch_counter += 1
 
 
 # Feed the input into the model
